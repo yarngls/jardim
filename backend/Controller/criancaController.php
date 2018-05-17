f2196117be94bb0d7c31bebd60ab59e4cb564c38
ANAS / GSI - Gilson Santos <gilson.ssantos@gov.cv>
2018-05-17 12:38:27 -0100

    change crianca revision34ttttttio9

--- backend/Controller/criancaController.php
+++ backend/Controller/criancaController.php
@@ -54,12 +54,12 @@
 			$dataInicioJardim			= 	@$crianca['dataInicioJardim'];
 			$estado						= 	@$crianca['estado'];
 
-			/*$insert_crianca = $db->query("INSERT into criancas(nome,sexo,dataNascimento,estado)
+			$insert_crianca = $db->query("INSERT into criancas(nome,sexo,dataNascimento,estado)
 								values('Jose Afonso','Masculino','2015-07-23','Matriculado');"); 
 							or die(mysqli_error($db));
 			$id_crianca=mysqli_insert_id($db);
 			$crianca["id"] = $id_crianca;
-			echo json_encode($crianca);*/
+			echo json_encode($crianca);
 		break;
 		/*case 'PUT':			
 			$crianca = json_decode(file_get_contents("php://input"), true);
