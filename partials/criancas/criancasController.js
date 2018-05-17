5e58215a6dfce808badea870ffb998edc4295c7c
ANAS / GSI - Gilson Santos <gilson.ssantos@gov.cv>
2018-05-17 11:23:36 -0100

    add input type profile image

--- partials/criancas/criancasController.js
+++ partials/criancas/criancasController.js
@@ -13,6 +13,47 @@ angular.module("app")
 
             simpleLoad(btn, false)
         });
+
+		$("#profileImage").click(function(e) {
+		    $("#imageUpload").click();
+		});
+
+		function fasterPreview( uploader ) {
+		    if ( uploader.files && uploader.files[0] ){
+		          $('#profileImage').attr('src', 
+		             window.URL.createObjectURL(uploader.files[0]) );
+		    }
+		}
+
+		$("#imageUpload").change(function(){
+		    fasterPreview( this );
+		});
+		
+
+        Dropzone.options.myAwesomeDropzone = {
+            autoProcessQueue: false,
+            uploadMultiple: true,
+            parallelUploads: 100,
+            maxFiles: 100,
+
+            // Dropzone settings
+            init: function() {
+                var myDropzone = this;
+
+                this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    myDropzone.processQueue();
+                });
+                this.on("sendingmultiple", function() {
+                });
+                this.on("successmultiple", function(files, response) {
+                });
+                this.on("errormultiple", function(files, response) {
+                });
+            }
+
+        }
   
 		function simpleLoad(btn, state) {
             if (state) {
