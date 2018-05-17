5659f4bbc3f9934faa641f58f7857cad02fb8b04
ANAS / GSI - Gilson Santos <gilson.ssantos@gov.cv>
2018-05-17 12:03:46 -0100

    change crianca revision

--- backend/Controller/criancaController.php
+++ backend/Controller/criancaController.php
@@ -10,7 +10,22 @@
 	switch ($want) {
 		
 		case 'GET':
-				echo json_encode("criancas");
+			
+				
+				$select_all=$db->query("SELECT  * FROM criancas where estado = 'Matriculado' ORDER BY nome ASC;");
+
+				$criancas = []; 
+
+				while ($data = mysqli_fetch_assoc($select_all))
+				{
+					//$data["nome"] = mb_convert_encoding($data["nome"], "UTF-8");
+					/*foreach ($data as $key => $value) {
+						$data[$key] = mb_convert_encoding($value, "UTF-8");
+					}*/
+					$criancas[] = $data;
+				}
+
+				echo json_encode($criancas);
 		break;
 		default:			
 			echo json_decode(["erro"=>"404"]);	
